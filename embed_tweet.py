6c78715bd2e4d06eefefe9c9293ee73240fa6416
Fundor333 <fundor333@gmail.com>
2017-12-05 21:00:47 +0100

    Reworking the plugin settings

--- embed_tweet.py
+++ embed_tweet.py
@@ -1,37 +1,47 @@
-"""
-Embedded tweet plugin for Pelican
-=================================
-
-This plugin allows you to embed Twitter tweets into your articles.
-And also provides a link for Twitter username.
-
-    i.e.
-
-        @username
-
-        will be replaced by a link to Twitter username page.
-
-        @username/status/tweetid
-
-        will be replaced by a `Embedded-tweet`_ API.
-
-.. _Embedded-tweet: https://dev.twitter.com/docs/embedded-tweets
-
-"""
 import re
-from pelican import signals
+from pelican import signals, logger
 
-def embed_tweet(content):
-    if not content._content:
+
+def embed_tweet(generator):
+
+    config_data = ""
+
+    if 'TWITTER_CARDS' in generator.settings:
+        config_data += " data-cards = '"+generator.settings["TWITTER_CARDS"]+"'"
+
+    if 'TWITTER_THEME' in generator.settings:
+        config_data += " data-theme = '"+generator.settings["TWITTER_THEME"]+"'"
+
+    if 'TWITTER_CONVERSATION' in generator.settings:
+        config_data += " data-conversation = '" + generator.settings["TWITTER_CONVERSATION"]+"'"
+
+    if 'TWITTER_LINK_COLOR' in generator.settings:
+        config_data += " data-link-color = '" + generator.settings["TWITTER_LINK_COLOR"]+"'"
+
+    if 'TWITTER_WIDTH' in generator.settings:
+        config_data += " data-width = '" + generator.settings["TWITTER_WIDTH"]+"'"
+
+    if 'TWITTER_ALIGN' in generator.settings:
+        config_data += " data-align = '" + generator.settings["TWITTER_ALIGN"]+"'"
+
+    if 'TWITTER_LANG' in generator.settings:
+        config_data += " data-lang = '" + generator.settings["TWITTER_LANG"]+"'"
+
+    if 'TWITTER_DNT' in generator.settings:
+        config_data += " data-dnt = '" + generator.settings["TWITTER_DNT"]+"'"
+
+    if not generator._content:
         return
 
-    content._content = re.sub(
+    generator._content = re.sub(
         r'(^|[^@\w])@(\w{1,15})\b',
-        '\\1<a href="https://twitter.com/\\2">@\\2</a>',
-        re.sub(
+    '\\1<a href="https://twitter.com/\\2">@\\2</a>',
+    re.sub(
             r'(^|[^@\w])@(\w{1,15})/status/(\d+)\b',
-            '\\1<blockquote class="twitter-tweet" align="center"><a href="https://twitter.com/\\2/status/\\3">Tweet of \\2/\\3</a></blockquote>',
-            content._content
+            '\\1<blockquote class="twitter-tweet" ' +
+            config_data +
+            ' align="center"><a href="https://twitter.com/\\2/status/\\3">Tweet of \\2/\\3</a></blockquote>',
+            generator._content
         )
     ) + '<script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>'
 
